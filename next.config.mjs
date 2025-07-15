d3fe9bdf24c7807fb3b8c637586d73c46c31dc8b
Guillermo Torres <torresrennerguilermo@gmai.com>
2025-07-14 22:09:20 -0400

    modificación tests

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
-    unoptimized: false,
+    unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? "/el-tomillo" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/el-tomillo/" : "",
 };
 
 export default nextConfig;
